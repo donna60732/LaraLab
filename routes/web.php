e677ee17086c6d1986a912ee2c2d9d3324242daf
donna60732 <donna60732@gmail.com>
2025-03-10 18:45:25 +0800

    建立jetstream / 建立model-Article

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticlesController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::resource('post', ArticlesController::class);
+Route::get('/', [ArticlesController::class, 'index'])->name('root');
+
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
